34fb9cb56132fb63250e93c47e2222964a1e8f84
Ritoban Goswami <ritobangoswami15@gmal.com>
2023-02-20 23:15:43 +0530

    first phase done

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,11 +1,47 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { blogList } from "../config/data.js";
+import { Link } from "react-router-dom";
+import EmptyList from "../components/Common/EmptyList";
+import Chip from "../components/Common/Chip";
+import "./BlogStyles.css";
 
 const Blog = () => {
   const { id } = useParams();
+  const [blog, setBlog] = useState(null);
+
+  useEffect(() => {
+    let blog = blogList.find((blog) => blog.id === parseInt(id));
+
+    if (blog) {
+      setBlog(blog);
+    }
+  }, []);
+
   return (
     <div>
-      <h1>Blog {id} Page</h1>
+      <Link className="blog-goBack" to={"/"}>
+        <span>&#8592; Go Back</span>
+      </Link>
+      {blog ? (
+        <div className="blog-wrap">
+          <header>
+            <p className="blog-date">Published {blog.createdAt}</p>
+            <h1>{blog.title}</h1>
+            <div className="blog-subCategory">
+              {blog.subCategory.map((category, index) => (
+                <div key={index}>
+                  <Chip label={category} />
+                </div>
+              ))}
+            </div>
+          </header>
+          <img src={blog.cover} alt="cover" />
+          <p className="blog-desc">{blog.description}</p>
+        </div>
+      ) : (
+        <EmptyList />
+      )}
     </div>
   );
 };
