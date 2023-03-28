335d15c0025dd31e3f9e5221c2de782d278a6b4f
Ritoban-Goswami <ritobangoswami15@gmail.com>
2023-03-28 19:51:56 +0530

    error handling on sign up

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -17,17 +17,16 @@ const SignIn = () => {
     setCreatingUser(true);
     await signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
-        // Signed in
         const user = userCredential.user;
         console.log(user);
         navigate("/");
-        // ...
       })
       .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.log(errorCode, errorMessage);
-        // ..
+        alert(errorMessage);
+        setCreatingUser(false);
       });
   };
 
