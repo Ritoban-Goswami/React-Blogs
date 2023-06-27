31b08cb493bbdc880d7f1e184ecebe79f83518b1
Ritoban Goswami <ritobangoswami15@gmal.com>
2023-06-27 21:09:46 +0530

    readme and small change on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,7 +4,7 @@ import BlogList from "../components/Home/BlogList";
 
 const Home = ({ blogs }) => {
   const sortedBlogsValue = Object.values(blogs).sort(
-    (a, b) => new Date(b.createdAt) - new Date(a.createdAt)
+    (a, b) => new Date(a.createdAt) - new Date(b.createdAt)
   );
   const sortedDataObj = sortedBlogsValue.reduce(
     (obj, item) => ({ ...obj, [item.id]: item }),
